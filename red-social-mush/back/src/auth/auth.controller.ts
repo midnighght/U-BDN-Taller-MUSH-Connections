58a6ecc6f86a2ce76c59df28a16b3f03097f84ed
martinUbilla <martin.ubilla@alumnos.ucn.cl>
2025-09-22 16:50:43 -0300

    jwt implementacion basica

---
+++ red-social-mush/back/src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, HttpCode, HttpStatus, NotImplementedException, Post, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { get } from 'mongoose';
+import { AuthGuard } from './guards/auth.guards';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(private authService: AuthService){}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() input: { username: string; password: string }) {
+        return this.authService.authenticate(input);
+        
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('me')
+    getUserInfo(@Request() request){
+        return request.user;
+    }
+
+}
